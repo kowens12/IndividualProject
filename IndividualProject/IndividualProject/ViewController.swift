9941d150e3a455f88bdcf625e4ded72c7fce9e5b
Kate <kowens711@gmail.com>
2015-11-02 15:59:25 -0500

    added draggable function to photo with uipangesturerecognizer

--- IndividualProject/IndividualProject/ViewController.swift
+++ IndividualProject/IndividualProject/ViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageContainer: UIImageView!
+    
+    @IBOutlet weak var dragImage: UIImageView!
+    let draggableImage = UIPanGestureRecognizer()
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageContainer.image = UIImage(named: "Image")
+        draggableImage.addTarget(self, action: "draggedView:")
+        dragImage.addGestureRecognizer(draggableImage)
+        dragImage.userInteractionEnabled = true
+
+
          // Do any additional setup after loading the view, typically from a nib.
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+   
+    func draggedView(sender: UIPanGestureRecognizer) {
+        self.view.bringSubviewToFront(sender.view!)
+        var translation = sender.translationInView(self.view)
+        sender.view!.center = CGPointMake(sender.view!.center.x + translation.x, sender.view!.center.y + translation.y)
+        sender.setTranslation(CGPointZero, inView: self.view)
+    }
 }
