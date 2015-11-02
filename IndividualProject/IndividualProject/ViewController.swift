39212a84592f01e5af4e6d80e7308df047fd576c
Kate <kowens711@gmail.com>
2015-11-02 10:19:00 -0500

    initial commit for repository

---
+++ IndividualProject/IndividualProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndividualProject
+//
+//  Created by Kate Owens  on 11/2/15.
+//  Copyright © 2015 Me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
