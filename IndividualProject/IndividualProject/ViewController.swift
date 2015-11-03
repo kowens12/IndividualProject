883c055e52c362705183055d58ae15695f173766
Kate <kowens711@gmail.com>
2015-11-03 09:30:11 -0500

    added uiimagepickercontroller functionality to import photo from device photo library

--- IndividualProject/IndividualProject/ViewController.swift
+++ IndividualProject/IndividualProject/ViewController.swift
@@ -8,23 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var dragImage: UIImageView!
     let draggableImage = UIPanGestureRecognizer()
 
-
+    @IBOutlet weak var userImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         draggableImage.addTarget(self, action: "draggedView:")
         dragImage.addGestureRecognizer(draggableImage)
         dragImage.userInteractionEnabled = true
-
-
-         // Do any additional setup after loading the view, typically from a nib.
-        
+        imagePicker.delegate = self
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -36,5 +35,32 @@ class ViewController: UIViewController {
         var translation = sender.translationInView(self.view)
         sender.view!.center = CGPointMake(sender.view!.center.x + translation.x, sender.view!.center.y + translation.y)
         sender.setTranslation(CGPointZero, inView: self.view)
+    }
+
+    
+    @IBAction func addUserImagePicker(sender: UIButton) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .PhotoLibrary
+      
+
+        presentViewController(imagePicker, animated: true, completion: nil)
+//        print("working")
+    }
+    
+    @IBAction func takeNewPhoto(sender: AnyObject) {
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("working")
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            print("working")
+            userImageView.contentMode = .ScaleAspectFit
+            userImageView.image = pickedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
